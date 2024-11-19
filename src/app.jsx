b2abcc246f88aca5e14da6cb5f5cd4a9d718012a
Jacob <Jacobemartinez2000@gmail.com>
2024-11-18 19:46:05 -0700

    Create App component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+    return (
+        <div>
+    <header>
+      <h1>TTRPG Inventory Manager</h1>
+      <nav>
+        <menu>
+          <li><a href="index.html">Login Page</a></li>
+          <li><a href="inventory.html">Inventory</a></li>
+          <li><a href="about.html">About</a></li>
+        </menu>
+      </nav>
+
+      <hr />
+    </header>
+ 
+        <main>App components go here</main>
+  
+    <footer>
+      <hr />
+      <span>Jacob E. Martinez</span>
+      <br />
+      <a href="https://github.com/Jacobadia/startup">GitHub</a>
+    </footer>
+  </div>
+    );
+  }
