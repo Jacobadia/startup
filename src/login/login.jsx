dcb51169a50902c2eadb10d994777e1790e4f701
Jacob <Jacobemartinez2000@gmail.com>
2024-12-03 13:02:52 -0700

    Mock Login Hooks

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -6,26 +6,51 @@ export function Login() {
   const [welcomeMessage, setWelcomeMessage] = useState('');
   const navigate = useNavigate();
 
-  const handleLoginOrCreate = () => {}
+  const handleLoginOrCreate = () => {
+    if (username.trim()) {
+      setWelcomeMessage(`Welcome ${username}`);
+    } else {
+      alert('Please enter a username.');
+    }
+  };
 
-  const handleContinue = () => {}
+  const handleContinue = () => {
+    navigate('/inventory');
+  };
 
 
   return (
     <main>
       <h1>Welcome to TTRPG Inventory</h1>
-      <form method="get" action="inventory.html">
+      {welcomeMessage ? (
         <div>
-          <span>Username</span>
-          <input type="text" placeholder="Username" />
+          <p>{welcomeMessage}</p>
+          <button onClick={handleContinue}>Continue</button>
+        </div>
+      ) : (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLoginOrCreate();
+          }}
+        >
+          <div>
+            <span>Username</span>
+            <input 
+            type="text" 
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.valueAsNumber)} 
+            />
         </div>
         <div>
           <span>Password</span>
           <input type="password" placeholder="password" />
         </div>
-        <button type="submit">Login</button>
-        <button type="submit">Create</button>
+        <button type="submit" onClick={handleLoginOrCreate}>Login</button>
+        <button type="submit" onClick={handleLoginOrCreate}>Create</button>
       </form>
+      )}
     </main>
   );
 }
